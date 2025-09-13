1db0a4a7e8f0ba6a8ca32bf1b55eb2a42b168bb7
Ricardo-Redondo <ricardo.spt.redondo@gmail.com>
2025-09-13 15:29:43 +0100

    42

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -5,90 +5,42 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rsao-pay <rsao-pay@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/12 00:13:20 by rsao-pay          #+#    #+#             */
-/*   Updated: 2025/09/12 10:20:10 by rsao-pay         ###   ########.fr       */
+/*   Created: 2025/09/11 16:07:06 by rsao-pay          #+#    #+#             */
+/*   Updated: 2025/09/13 15:29:08 by rsao-pay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_comb2(void)
+void	ft_print_combo2(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	int		i;
+	char	a[2];
+	int		v;
+	char	b[2];
 
-	a = '49';
-	b = '48';
-	c = '48';
-	d = '48';
-
-	char a1;
-	char b1;
-	char c1;
-	char d1;
-
-	a1 = a;
-	b1 = b;
-	c1 = c;
-	d1 = d;
-
-	while (d <= '57')
+	i = 0;
+	while (i <= 98)
 	{
-		while (c <= '57')
+		v = i + 1;
+		while (v <= 99)
 		{
-			while (b <= '57')
-			{
-				while (a <= '57')
-				{
-					write(1, &d1, 1);
-					write(1, &c1, 1);
-					write(1, &b1, 1);
-					write(1, &a1, 1);
-					a++;
-					a1 = a;
-				}
-
-				write(1, &d1, 1);
-				write(1, &c1, 1);
-				write(1, &b1, 1);
-				write(1, &a1, 1);
-				b++;
-				b1 = b;
-				a = '48';
-				a1 = a;
-				
-			}
-
-			write(1, &d1, 1);
-			write(1, &c1, 1);
-			write(1, &b1, 1);
-			write(1, &a1, 1);
-			d++;
-			d1 = d;
-			a = '48';
-			a1 = a;
-			b = '48';
-			b1 = b;
+			a[0] = '0' + (i / 10);
+			a[1] = '0' + (i % 10);
+			b[0] = '0' + (v / 10);
+			b[1] = '0' + (v % 10);
+			write(1, a, 2);
+			write(1, " ", 1);
+			write(1, b, 2);
+			if (!(i == 98 && v == 99))
+				write(1, ", ", 2);
+			v++;
 		}
-		write(1, &d1, 1);
-		write(1, &c1, 1);
-		write(1, &b1, 1);
-		write(1, &a1, 1);
-		c++;
-		c1 = c;
-		a = '48';
-		a1 = a;
-		b = '48';
-		b1 = d;
-		d = '48';
-		d1 = d;
+		i++;
 	}
-
 }
 
-int main()
-{
-	ft_print_comb2();
-}
+// int	main(void)
+// {
+// 	ft_print_combo2();
+// }
