dfcfc517b2da7526f924c17ade849502467a0517
Ricardo Sao Payo Teixeira Redondo <rsao-pay@c1r2s9.42lisboa.com>
2025-09-15 16:41:58 +0100

    42

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsao-pay <rsao-pay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/15 16:00:02 by rsao-pay          #+#    #+#             */
+/*   Updated: 2025/09/15 16:39:58 by rsao-pay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int i;
+	int temp;
+	
+	i = 0;
+	while(tab[i] != size/2)
+	{
+		temp = tab[i];
+		tab[i] = tab[size -1 -i];
+		tab[size -1 -i] = temp;
+		i++;
+	}
+}
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size);
+int main()
+{
+	int tab[]={1, 2, 3, 4, 5};
+	int size = 5;
+	ft_rev_int_tab(tab, size);
+	printf("%d", ft_rev_int_tab);
+	return (0);
+}
