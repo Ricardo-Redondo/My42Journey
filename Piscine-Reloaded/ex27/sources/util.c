c7a17df3cf897c6d3c3677b5396b55f47775a16a
Ricardo-Redondo <ricardo.spt.redondo@gmail.com>
2025-10-12 17:41:04 +0100

    42

--- Piscine-Reloaded/ex27/sources/util.c
+++ Piscine-Reloaded/ex27/sources/util.c
@@ -6,7 +6,7 @@
 /*   By: rsao-pay <rsao-pay@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 17:16:09 by rsao-pay          #+#    #+#             */
-/*   Updated: 2025/10/12 17:30:11 by rsao-pay         ###   ########.fr       */
+/*   Updated: 2025/10/12 17:40:51 by rsao-pay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	display_file(int fd)
 	int bytes;
 
 	bytes = read(fd, buffer, sizeof(buffer));
-	while ((bytes = read(fd, buffer, sizeof(buffer))) > 0)
-		write(1, buffer, bytes);
 	if (bytes < 0)
 	{
 		write(2, "Cannot read file.\n", 18);
 		return;
 	}
+	while ((bytes = read(fd, buffer, sizeof(buffer))) > 0)
+		write(1, buffer, bytes);
 }
